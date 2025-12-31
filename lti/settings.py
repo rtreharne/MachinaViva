aabbc9c94e10060c2170b7ef60d3c5938457e88c
linus <rob.treharne@gmail.com>
2025-12-31 07:27:06 +0000

    fixing brevo email issue

--- lti/settings.py
+++ lti/settings.py
@@ -163,6 +163,11 @@ MEDIA_ROOT = Path(os.getenv("MEDIA_ROOT", BASE_DIR / "media"))
 # ----------------------------------------------------
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "no-reply@machinaviva.local")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "localhost")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "25"))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "false").strip().lower() in ["1", "true", "yes", "on"]
 
 
 # ----------------------------------------------------
